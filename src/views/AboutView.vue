7dfdd50288641a5940d2034a5ef8617f065be212
ramiahmed68 <108447976+Rami-Ahmed68@users.noreply.github.com>
2024-05-26 07:23:51 -0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,59 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="div">
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+      <div class="child">cont</div>
+    </div>
   </div>
 </template>
+
+
+<style scoped>
+.div {
+  width: 100%;
+  background-color: green;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  overflow-x: scroll;
+}
+
+.child {
+  width: 100px;
+  height: 100px;
+  margin: 10px;
+  background-color: red;
+}
+</style>
